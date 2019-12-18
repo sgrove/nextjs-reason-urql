fbff27c3da6b63112abaca37210788858137f53c
Sean Grove <sean@bushi.do>
2019-12-18 10:33:47 -0800

    Fix up es6 modules

--- next.config.js
+++ next.config.js
@@ -1,11 +1,15 @@
 const bsconfig = require('./bsconfig.json');
 const withCSS = require('@zeit/next-css');
 const withTM = require("next-transpile-modules");
+
+// Add all dependencies built with es6 target here, otherwise
+// Nexts' babel plugin doesn't load it properly
+const es6NodeModules = ["wonka"];
+
 const config = {
   target: 'serverless',
   pageExtensions: ["jsx", "js", "bs.js"],
-  transpileModules: ["bs-platform"].concat(bsconfig["bs-dependencies"])
+  transpileModules: ["bs-platform"].concat(es6NodeModules).concat(bsconfig["bs-dependencies"])
 };
 
 module.exports = withTM(withCSS(config));
-
